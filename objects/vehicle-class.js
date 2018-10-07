fb0ab20f6331c120cf0a2d4a38b1f7b00e47d4b0
hai.t.le16@gmail.com <hai.t.le16@gmail.com>
2018-10-06 17:57:22 -0700

    object lab finished

--- objects/vehicle-class.js
+++ objects/vehicle-class.js
@@ -1,6 +1,34 @@
 'use strict';
 
-class Car {
+class Vehicle{
+    constructor(name, wheels){
+        this.name = name;
+        this.wheels = wheels;
+    }
+    drive(){
+        return 'Moving Forward';
+    }
+    stop(){
+        return 'Stopping';
+    }
+}
+class Car extends Vehicle {
+    constructor(name) {
+        super();
+        this.name = name;
+        super.wheels = 4;
+    }
 }
 
-module.exports = Car;
+class Motorcycle extends Vehicle{
+    constructor(name) {
+        super();
+        this.name = name;
+        super.wheels = 2;
+    }
+    wheelie(){
+        return 'Whee!';
+    }
+}
+
+module.exports = {Car, Motorcycle};
